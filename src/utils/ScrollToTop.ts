c5d771c8be0d8918086246b72474f05efd246fdd
jackyonderbrack <mike.projektowanie@gmail.com>
2024-01-03 11:16:44 +0100

    feat: smooth behavior scroll added

--- src/utils/ScrollToTop.ts
+++ src/utils/ScrollToTop.ts
@@ -5,7 +5,11 @@ export const ScrollToTop = () => {
   const location = useLocation();
 
   useEffect(() => {
-    window.scrollTo(0, 0);
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
   }, [location]);
 
   return null;
