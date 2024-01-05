0119a7e83f5a0c3f0b8f9a9b00ce596985c9b167
jackyonderbrack <mike.projektowanie@gmail.com>
2024-01-05 07:27:14 +0100

    smooth animation to button added

--- src/utils/AniTransition/useAnimateTransition.tsx
+++ src/utils/AniTransition/useAnimateTransition.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const useAnimateTransition = (duration = 300) => {
+const useAnimateTransition = (duration = 120) => {
   const [transitionStage, setTransitionStage] = useState("enter");
 
   const triggerAnimation = () => {
